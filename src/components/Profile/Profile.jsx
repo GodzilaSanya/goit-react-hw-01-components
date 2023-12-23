4052a0f0fc6dec883e146f5dcbc205be6d38cc4a
GodzilaSanya <godzilasanya@gmail.com>
2023-12-23 11:40:01 +0200

    add start files

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+import './profile.css';
+
+const Profile = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <p></p>
+      <span></span>
+      <span></span>
+      <ul>
+        <li>
+          <p></p>
+          <span></span>
+        </li>
+        <li>
+          <p></p>
+          <span></span>
+        </li>
+        <li>
+          <p></p>
+          <span></span>
+        </li>
+      </ul>
+      <p className="p-color">it's placeholder for markup Profile</p>
+    </div>
+  );
+};
+export default Profile;
