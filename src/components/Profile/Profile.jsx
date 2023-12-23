38d21667f320ded6a8f8c02991b42ad23ad39776
GodzilaSanya <godzilasanya@gmail.com>
2023-12-23 16:12:46 +0200

    added styles element profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,15 +11,15 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
       </div>
 
       <ul className="stats">
-        <li>
+        <li className="stats-item">
           <span className="label">Followers</span>
           <span className="quantity"> {stats.followers}</span>
         </li>
-        <li>
+        <li className="stats-item">
           <span className="label">Views</span>
           <span className="quantity"> {stats.views}</span>
         </li>
-        <li>
+        <li className="stats-item">
           <span className="label">Likes</span>
           <span className="quantity"> {stats.likes}</span>
         </li>
