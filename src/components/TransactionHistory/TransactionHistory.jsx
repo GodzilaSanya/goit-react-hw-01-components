44deea03b86dd34c0259d2d686f8de256a1c9345
GodzilaSanya <godzilasanya@gmail.com>
2023-12-24 09:18:07 +0200

    added Transaction component

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,40 @@
+import css from './transactionHistory.module.css';
+
+const white = '#ffffff';
+const grey = '#ecf1f3';
+let currentColor = grey;
+
+const TransactionHistory = ({ items }) => {
+  const tableRows = items.map(({ id, type, amount, currency }) => {
+    return (
+      <tr key={id} style={{ backgroundColor: colorizeTable() }}>
+        <td>{type}</td>
+        <td>{amount}</td>
+        <td>{currency}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table className={css.transaction_history}>
+      <thead className={css.table_head}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>{tableRows}</tbody>
+    </table>
+  );
+};
+
+function colorizeTable() {
+  if (currentColor === grey) {
+    return (currentColor = white);
+  } else {
+    return (currentColor = grey);
+  }
+}
+
+export default TransactionHistory;
